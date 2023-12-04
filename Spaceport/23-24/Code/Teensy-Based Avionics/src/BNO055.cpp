064edca231c1b499752f87d04b41570c18d4af0a
Azhanzz <146783694+Azhanzz@users.noreply.github.com>
2023-12-04 14:54:15 -0500

    Update BNO055.cpp

--- Spaceport/23-24/Code/Teensy-Based Avionics/src/BNO055.cpp
+++ Spaceport/23-24/Code/Teensy-Based Avionics/src/BNO055.cpp
@@ -13,19 +13,14 @@ acceleraton_vec = getVector(VECTOR_ACCELEROMETER);
 return acceleration_vec
 }
 
-
 virtual imu::Vector<3> get_orientation_euler() {
 euler_vec = getVector(EULER);
 return euler_vec;
 }
 
-
-
-
 virtual String getdataString() {
 return String(accleration_vec.x) +”,” + String(accleration_vec.y) +”,” +String(accleration_vec.z) +”,” + String(euler_vec.x) +”,” + String(euler_vec.y) +”,” String(euler_vec.z);
 } 
-
 
 virtual String getcsvHeader() {
 String s = “acceleration_X (m/s/s), acceleration_Y (m/s/s), acceleration_Z (m/s/s) **** euler (orientation x), euler (orientation y), euler (orientation z)”;
