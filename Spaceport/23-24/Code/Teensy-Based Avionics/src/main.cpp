eac0ef24051192d84eb3e100e90087a4443c569c
Drew Brandt <ahbrandt@umd.edu>
2024-06-18 20:37:37 -0400

    changed radio freq

--- Spaceport/23-24/Code/Teensy-Based Avionics/src/main.cpp
+++ Spaceport/23-24/Code/Teensy-Based Avionics/src/main.cpp
@@ -18,7 +18,7 @@ BNO055 bno(13, 12);         // I2C Address 0x29
 BMP390 bmp(13, 12);         // I2C Address 0x77
 MAX_M10S gps(13, 12, 0x42); // I2C Address 0x42
 
-RadioSettings settings = {915.0, 0x01, 0x02, &hardware_spi, 10, 31, 32};
+RadioSettings settings = {433.78, 0x01, 0x02, &hardware_spi, 10, 31, 32};
 RFM69HCW radio(&settings);
 APRSHeader header = {"KC3UTM", "APRS", "WIDE1-1", '^', 'M'};
 APRSCmdData currentCmdData = {800000, 800000, 800000, false};
